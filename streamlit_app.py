cd23a56559fddd791fdbfd9c495674d6a129f14a
Eduardo Félix <62554540+EduardoFelixNeto@users.noreply.github.com>
2023-04-05 08:46:30 -0300

    Update streamlit_app.py
    Added some recipes for breakfast.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Brakfast Menu')
+streamlit.text('Eggs and Bacon')
+strealit.text('Banana, Grapes, Apples and Honey')
+strealit.text('Oatmeal and Milk')
