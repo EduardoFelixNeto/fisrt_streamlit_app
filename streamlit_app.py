424c0fefe2735baff344b995b35ce6f96d6d744f
Eduardo Félix <62554540+EduardoFelixNeto@users.noreply.github.com>
2023-04-05 08:46:57 -0300

    Update streamlit_app.py
    Fixed a typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Brakfast Menu')
 streamlit.text('Eggs and Bacon')
-strealit.text('Banana, Grapes, Apples and Honey')
-strealit.text('Oatmeal and Milk')
+streamlit.text('Banana, Grapes, Apples and Honey')
+streamlit.text('Oatmeal and Milk')
