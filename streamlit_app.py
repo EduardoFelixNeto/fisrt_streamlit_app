6b3130578e4fcb5ae8255ed409660bde3e863f51
Eduardo Félix <62554540+EduardoFelixNeto@users.noreply.github.com>
2023-04-05 08:50:17 -0300

    Update streamlit_app.py
    Added some emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Brakfast Menu')
-streamlit.text('Eggs and Bacon')
-streamlit.text('Banana, Grapes, Apples and Honey')
-streamlit.text('Oatmeal and Milk')
+streamlit.header('Breakfast Menu 🍞')
+streamlit.text('🐔 Eggs and Bacon')
+streamlit.text('🥑 Banana, Grapes, Apples and Honey')
+streamlit.text('🥗 Oatmeal and Milk')  
+streamlit.header('Build your own fruit smoothie')
