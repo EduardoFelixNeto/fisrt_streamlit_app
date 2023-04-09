fce31a6d6cef2e9d1292ef6a96ee32746d9f70ce
Eduardo Félix <62554540+EduardoFelixNeto@users.noreply.github.com>
2023-04-09 20:18:11 -0300

    Update streamlit_app.py
    added a title

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,8 @@ def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
     return my_cur.fetchall()
-if streamlit.button('Get fruit load list'):
+streamlit.header("View Our fruit list - Add a new one")
+if streamlit.button('Get fruit list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
